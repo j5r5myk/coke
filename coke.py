9eef689faceed5e9619b2c733591dca7a7c2d319
Jeremy Kleiman <j5r5myk@gmail.com>
2016-03-26 17:28:26 -0400

    added command line arguments

--- coke.py
+++ coke.py
@@ -1,20 +1,22 @@
 import sys
 import requests
 
-path = "/usr/share/dict/words"
+path = '/usr/share/dict/words'
+country = 'US'
 
-print len(sys.argv)
-'''
 if len(sys.argv) > 1:
   path = sys.argv[1]
-'''
+
+if len(sys.argv) > 2:
+  country = sys.argv[2]
+
 # Build list of words
 with open(path, 'r') as f:
   words = [line.strip() for line in f]
 
 # Test words
 for word in words:
-  payload = {'term': word, 'country': 'US'}
+  payload = {'term': word, 'country': country}
   r = requests.get('https://tastethefeeling.coca-cola.com/api/profanity', params=payload)
   if len(r.content) != 2:
     print word, " is blocked."
